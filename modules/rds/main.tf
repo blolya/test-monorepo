762576fdf168478e977523d96e3a51b0cbad3b49
blolya <mast-94@mail.ru>
2023-04-26 12:08:38 +0300

    add: github actions config file; change: terraform database config to use environment variables

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,12 +1,12 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  db_name              = "test"
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  skip_final_snapshot  = true
-  parameter_group_name = "default.postgres14"
-  publicly_accessible = true
+  allocated_storage      = 5
+  db_name                = var.DB_NAME
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  username               = var.DB_USER
+  password               = var.DB_PASSWORD
+  skip_final_snapshot    = true
+  parameter_group_name   = "default.postgres14"
+  publicly_accessible    = true
   vpc_security_group_ids = [var.vpc_security_group_id]
 }
--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,1 +1,16 @@
 variable "vpc_security_group_id" {}
+
+variable "DB_NAME" {
+  type = string
+  description = "Database name"
+}
+
+variable "DB_USER" {
+  type = string
+  description = "Database user name"
+}
+
+variable "DB_PASSWORD" {
+  type = string
+  description = "Database password"
+}
